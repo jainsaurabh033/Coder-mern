7f7840d034762b91bae46e2a3b7fcc61c7705310
saurabh <saurabhjain04r@gmail.com>
2023-04-16 21:51:04 +0530

    part 5

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.js
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../../../components/Shared/Card/Card";
+import Button from "../../../components/Shared/Button/Button";
+import styles from "./StepAvatar.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { setAvatar } from "../../../store/activateSlice";
+import { activate } from "../../../http";
+import { setAuth } from "../../../store/authSlice";
 
 const StepAvatar = ({ onNext }) => {
+  const dispatch = useDispatch();
+  const { name, avatar } = useSelector((state) => state.activate);
+  const [image, setImage] = useState("/img/monkey-avatar.png");
+
+  function captureImage(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      setImage(reader.result);
+      dispatch(setAvatar(reader.result));
+    };
+  }
+
+  async function submit() {
+    try {
+      const { data } = await activate({ name, avatar });
+      if (data.auth) {
+        dispatch(setAuth(data));
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <>
-      <div>
-        <h1>Avatar</h1>
-      </div>
-      <button onClick={onNext}>Next</button>
+      <Card title={`Okay, ${name}`} icons="monkey-emoji">
+        <p className={styles.subHeading}>How's this photo</p>
+        <div className={styles.avatarWrapper}>
+          <img className={styles.avatarImage} src={image} alt="avatar" />
+        </div>
+        <div>
+          <input
+            onChange={captureImage}
+            id="avatarInput"
+            type="file"
+            className={styles.avatarInput}
+          />
+          <label className={styles.avatarLabel} htmlFor="avatarInput">
+            Choose a different photo
+          </label>
+        </div>
+
+        <div>
+          <Button onClick={submit} text="Next" />
+        </div>
+      </Card>
     </>
   );
 };
--- frontend/src/pages/Steps/StepName/StepName.js
+++ frontend/src/pages/Steps/StepName/StepName.js
@@ -1,12 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Card from "../../../components/Shared/Card/Card";
+import Button from "../../../components/Shared/Button/Button";
+import TextInput from "../../../components/Shared/TextInput/TextInput";
+import { useDispatch, useSelector } from "react-redux";
+import { setName } from "../../../store/activateSlice";
+import styles from "./StepName.module.css";
 const StepName = ({ onNext }) => {
+  const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.activate);
+  const [fullname, setFullname] = useState(name);
+
+  function nextStep() {
+    if (!fullname) {
+      return;
+    }
+    dispatch(setName(fullname));
+    onNext();
+  }
+
   return (
     <>
-      <div>
-        <h1>Name component</h1>
-      </div>
-      <button onClick={onNext}>Next</button>
+      <Card title="What's your full name?" icons="goggle-emoji">
+        <TextInput
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+        />
+        <p className={styles.paragraph}>
+          People use real names at coderhouse :) !
+        </p>
+        <div>
+          <Button onClick={nextStep} text="Next" />
+        </div>
+      </Card>
     </>
   );
 };
