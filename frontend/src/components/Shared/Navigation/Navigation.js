b0d5907b6af30281f44b8ca11a7ab132d9b42a35
saurabh <saurabhjain04r@gmail.com>
2023-04-19 20:13:32 +0530

    created the rooms page and directed to the room page

--- frontend/src/components/Shared/Navigation/Navigation.js
+++ frontend/src/components/Shared/Navigation/Navigation.js
@@ -37,23 +37,24 @@ const Navigation = () => {
         <img src="/img/logo.png" alt="logo" />
         <span style={logoText}>Coderhouse</span>
       </Link>
-      <div className={styles.navRight}>
-        <h3>{user.name}</h3>
-        <Link to="/">
-          <img
-            className={styles.avatar}
-            src={user.avatar}
-            width="40"
-            height="40"
-            alt="avatar"
-          />
-          {isAuth && (
-            <button className={styles.logoutButton} onClick={logoutUser}>
-              <img src="/img/logout.png" alt="logout" />
-            </button>
-          )}
-        </Link>
-      </div>
+      {isAuth && (
+        <div className={styles.navRight}>
+          <h3>{user?.name}</h3>
+
+          <Link to="/">
+            <img
+              className={styles.avatar}
+              src={user.avatar ? user.avatar : "/images/monkey-avatar.png"}
+              width="40"
+              height="40"
+              alt="avatar"
+            />
+          </Link>
+          <button className={styles.logoutButton} onClick={logoutUser}>
+            <img src="/img/logout.png" alt="logout" />
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
